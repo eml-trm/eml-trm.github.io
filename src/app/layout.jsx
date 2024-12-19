caf35bb2a11deb8fc0b4afc3e67def1d70aacde2
eml-trm <termeau.e@gmail.com>
2024-12-19 14:30:59 +0100

    Fix case in metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,7 +6,7 @@ import { Amaranth } from 'next/font/google'
 const inter = Amaranth({ subsets: ['latin'], weight: ['400', '700'], })
 
 export const metadata = {
-  title: 'assistante numérique à domicile',
+  title: 'Assistante numérique à domicile',
   description: 'Services d\'accompagnement numérique personnalisés à domicile',
 }
 
